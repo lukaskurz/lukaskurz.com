79c1b2a03af5eae904cb9313912c5410f162f5aa
lukaskurz <lukaskurz5@gmail.com>
2019-04-21 01:48:31 +0200

    added coloring

--- src/app/hover-link/hover-link.component.ts
+++ src/app/hover-link/hover-link.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-hover-link',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hover-link.component.scss']
 })
 export class HoverLinkComponent implements OnInit {
+
+  @Input() color: 'red' | 'blue' |'green' = 'red';
+
 
   constructor() { }
 
