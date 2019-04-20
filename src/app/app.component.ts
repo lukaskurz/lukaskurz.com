67ad5c20bc5d821005bdb9150c3e3cae6d6c1003
lukaskurz <lukaskurz5@gmail.com>
2019-04-20 21:31:45 +0200

    restructered and added about component + animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,83 @@
-import {Component} from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  query,
+  transition,
+  animateChild,
+  group
+} from '@angular/animations';
+import { RouterOutlet } from '@angular/router';
+
+export const slideInAnimation =
+  trigger('routeAnimations', [
+    transition('greeting => about', [
+      style({ position: 'relative' }),
+      query(':enter, :leave', [
+        style({
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%'
+        })
+      ]),
+      query(':enter', [
+        style({ top: '100vh' })
+      ]),
+      query(':leave', animateChild()),
+      group([
+        query(':leave', [
+          animate('600ms cubic-bezier(.49,-0.01,.1,.69)', style({ top: '-100vh' }))
+        ]),
+        query(':enter', [
+          animate('600ms cubic-bezier(.49,-0.01,.1,.69)', style({ top: '0vh' }))
+        ])
+      ]),
+      query(':enter', animateChild()),
+    ]),
+    transition('about => greeting', [
+      style({ position: 'relative' }),
+      query(':enter, :leave', [
+        style({
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%'
+        })
+      ]),
+      query(':enter', [
+        style({ top: '-100vh' })
+      ]),
+      query(':leave', animateChild()),
+      group([
+        query(':leave', [
+          animate('300ms cubic-bezier(.49,-0.01,.1,.69)', style({ top: '100vh' }))
+        ]),
+        query(':enter', [
+          animate('300ms cubic-bezier(.49,-0.01,.1,.69)', style({ top: '0vh' }))
+        ])
+      ]),
+      query(':enter', animateChild()),
+    ]),
+  ]);
+
 
 @Component({
   selector: 'app-root',
-  template: `<router-outlet></router-outlet>`,
+  template: `
+  <div [@routeAnimations]="prepareRoute(outlet)" >
+    <router-outlet #outlet="outlet"></router-outlet>
+  </div>
+`,
+  animations: [
+    slideInAnimation
+  ]
 })
-export class AppComponent {
 
+export class AppComponent {
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,28 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-import {AppComponent} from './app.component';
-import {HomeComponent} from './home/home.component';
-import {TransferHttpCacheModule} from '@nguniversal/common';
-import { GreetingComponent } from './home/greeting/greeting.component';
+import { AppComponent } from './app.component';
+import { TransferHttpCacheModule } from '@nguniversal/common';
+import { GreetingComponent } from './greeting/greeting.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     GreetingComponent,
+    AboutComponent,
   ],
   imports: [
-    BrowserModule.withServerTransition({appId: 'my-app'}),
+    BrowserModule.withServerTransition({ appId: 'my-app' }),
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full'},
-      { path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule'},
-      { path: 'lazy/nested', loadChildren: './lazy/lazy.module#LazyModule'}
+      { path: '', redirectTo: 'greeting', pathMatch: 'full' },
+      { path: 'greeting', component: GreetingComponent, data: {animation: 'greeting'} },
+      { path: 'about', component: AboutComponent, data: {animation: 'about'} }
     ]),
     TransferHttpCacheModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
