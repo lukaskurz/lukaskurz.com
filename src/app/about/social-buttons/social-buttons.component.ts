2e2bf8d506d17ccab250717850e815b205e63a9e
lukaskurz <lukaskurz5@gmail.com>
2019-04-22 16:30:02 +0200

    added social buttons

--- src/app/about/social-buttons/social-buttons.component.ts
+++ src/app/about/social-buttons/social-buttons.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faGithub, faLinkedinIn, faRedditAlien, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-social-buttons',
@@ -6,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./social-buttons.component.scss']
 })
 export class SocialButtonsComponent implements OnInit {
+
+  faGithub = faGithub;
+  faLinkedinIn = faLinkedinIn;
+  faEnvelope = faEnvelope;
+  faRedditAlien = faRedditAlien;
+  faYoutube = faYoutube;
 
   constructor() { }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { AboutComponent } from './about/about.component';
 import { HoverLinkComponent } from './hover-link/hover-link.component';
 import { SkillListComponent } from './skills/skill-list/skill-list.component';
 import { SocialButtonsComponent } from './about/social-buttons/social-buttons.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @NgModule({
   declarations: [
@@ -24,13 +26,14 @@ import { SocialButtonsComponent } from './about/social-buttons/social-buttons.co
     BrowserModule.withServerTransition({ appId: 'my-app' }),
     RouterModule.forRoot([
       { path: '', redirectTo: 'greeting', pathMatch: 'full' },
-      { path: 'greeting', component: GreetingComponent, data: {animation: 'greeting'} },
-      { path: 'about', component: AboutComponent, data: {animation: 'about'} },
-      { path: 'skills', component: SkillListComponent, data: {animation: 'skills'} },
-      { path: 'projects', loadChildren: './projects/projects.module#ProjectsModule', data: {animation: 'projects'} }
+      { path: 'greeting', component: GreetingComponent, data: { animation: 'greeting' } },
+      { path: 'about', component: AboutComponent, data: { animation: 'about' } },
+      { path: 'skills', component: SkillListComponent, data: { animation: 'skills' } },
+      { path: 'projects', loadChildren: './projects/projects.module#ProjectsModule', data: { animation: 'projects' } }
     ]),
     TransferHttpCacheModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
